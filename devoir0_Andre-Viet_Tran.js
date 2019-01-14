82a2adc0cb2f72657773116eb008b98a03882f7d
Andre Tran <andreviet2009@hotmail.com>
2019-01-14 15:32:39 -0500

    program

---
+++ devoir0_Andre-Viet_Tran.js
@@ -0,0 +1,11 @@
+//Andre-Viet Tran 
+
+var prenom = "Andre-Viet ", nom = "Tran";
+var salutation = "Bonjour, mon nom est " + prenom + nom +"!";
+
+var age = 22;;
+var discipline = "informatique";
+var informations = "J'ai " + age + "ans et j'etudie en " + discipline + ".";
+
+var introduction = salutation+ "\n" + informations;
+print(introduction);
